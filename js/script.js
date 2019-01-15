202c2dfc1d430c3f84294b74ab6fc48195d04734
Vinicius Donadon <vinicius.donadon@sunsetcom.com.br>
2019-01-15 15:19:09 -0200

    Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,61 @@
+// Selecionando todos os itens
+const slide = document.querySelector('.slide')
+
+// Tamanho da margem
+const margin = 10
+const doubMargin = margin * 2
+
+// Quantidade de itens 
+let qnt = 2;
+let qntItem = document.querySelectorAll('.slide li')
+qntItem.forEach((element) => {
+    element.setAttribute("style", `width:${100/qnt}%; margin-left: ${margin}px; margin-right: ${margin}px`)
+    element.setAttribute("style", `width:${100/qnt}%; margin-left: ${margin}px; margin-right: ${margin}px`)
+})
+
+// Se objeto for loop falso
+// Loop false
+let totalItem = 0;
+let eachItem = 0;
+let larguraMaxima = document.querySelectorAll('.slide li')
+let length = larguraMaxima.length
+larguraMaxima.forEach((element) => {
+    eachItem = element.offsetWidth
+    totalItem = totalItem + eachItem + doubMargin
+})
+
+// Se objeto for loop true
+
+
+// Tamanho dos itens
+let item = document.querySelector('.slide li')
+item = item.offsetWidth
+
+// Variavel para armazenar valor final
+let total = 0
+let itens = 0
+
+// Selecionando o botão esquerdo
+const lButton = document.querySelector('.left-button')
+lButton.addEventListener('click', function(){
+    if(itens > 0){
+    total += item + doubMargin
+    itens--
+    slide.setAttribute("style", `transform: translateX(${total}px)`)
+    } else {
+        total -= 0
+    }
+})
+
+// Selecionando o botão direito
+const rButton = document.querySelector('.right-button')
+rButton.addEventListener('click', function(){
+    if(itens < (larguraMaxima.length - 1)){
+        total -= item + doubMargin
+        itens++
+        slide.setAttribute("style", `transform: translateX(${total}px)`)
+    } else {
+        total += 0
+    }
+})
+
