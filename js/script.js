e17da22bbfe13e895a711b2a174b858bb36d77be
Vinicius Donadon <biumdonadon@hotmail.com>
2019-01-18 15:39:54 -0200

    Semi Finalizado: Sem Loop e touch

--- js/script.js
+++ js/script.js
@@ -18,12 +18,19 @@ const Slider = function (value) {
 
     // Item size
     let WindowInner = 0;
-    WindowInner = (WindowSize / ItemsOnScreen) - DoubleMargin;
+    WindowInner = (WindowSize / ItemsOnScreen);
 
     // Stores the Element Width
     SlideArray.forEach((element) => {
-        element.setAttribute("style", `width: ${WindowInner}px; margin-left: ${Margin}px; margin-right: ${Margin}px`)
+        element.setAttribute("style", `width: ${WindowInner - (Margin + (Margin/ItemsOnScreen))}px; margin-right: ${Margin}px`)
     });
+
+    // Setting the margin for first and last child
+    const FirstChild = SlideArray[0]
+    FirstChild.setAttribute("style", `width: ${WindowInner - (Margin + (Margin/ItemsOnScreen)) }px; margin-right: ${Margin}px; margin-left: ${Margin}`)
+
+    // const LastChild = SlideArray[SlideArray.length - 1]
+    // LastChild.setAttribute("style", `width: ${WindowInner - (Margin + (Margin/ItemsOnScreen))}px; margin-right: ${Margin}px; margin-left: ${Margin}`)
 
     // Array Length
     let SlideLength = SlideArray.length
@@ -36,27 +43,29 @@ const Slider = function (value) {
 
     // Slider Buttons
     const LeftButton = document.querySelector('.left-button')
+    LeftButton.setAttribute("style", `opacity: 0.4; cursor: no-drop`)
     LeftButton.addEventListener('click', function () {
-        RightButton.setAttribute("style", `opacity: 1; cursor: pointer`)
+        RightButton.setAttribute("style", `opacity: 1`)
         if (Counter > 0 ) {
-            TranslateX += WindowSize / ItemsOnScreen
+            if(Counter == 1){
+                LeftButton  .setAttribute("style", `opacity: 0.4; cursor: no-drop`)    
+            }
+            TranslateX += (WindowSize / ItemsOnScreen) - (Margin/ItemsOnScreen)
             Counter--
             SlideUL.setAttribute("style", `transform: translateX(${TranslateX}px)`)
-        } else {
-            LeftButton.setAttribute("style", `opacity: 0.4; cursor: no-drop`)
         }
     });
 
     const RightButton = document.querySelector('.right-button')
     RightButton.addEventListener('click', function () {
-        LeftButton.setAttribute("style", `opacity: 1; cursor: pointer`)
-        if (Counter < (SlideLength - ItemsOnScreen)) {
-            RightButton.setAttribute("style", `opacity: 1; cursor: pointer`)
-            TranslateX -= WindowSize / ItemsOnScreen
+        LeftButton.setAttribute("style", `opacity: 1; cursor: no-drop`)
+        if (Counter <= ((SlideLength - ItemsOnScreen) -1 )) {
+            if(Counter == (SlideLength - ItemsOnScreen) - 1){
+                    RightButton.setAttribute("style", `opacity: 0.4; cursor: no-drop`)    
+            }
+            TranslateX -= (WindowSize / ItemsOnScreen) - (Margin/ItemsOnScreen)
             Counter++
             SlideUL.setAttribute("style", `transform: translateX(${TranslateX}px)`)
-        } else {
-            RightButton.setAttribute("style", `opacity: 0.4; cursor: no-drop`)
         }
     })
 
@@ -79,25 +88,18 @@ const Slider = function (value) {
         let Counter = 0;
 
         RightButton.addEventListener('click', function () {
-            LeftButton.setAttribute("style", `opacity: 1; cursor: pointer`)
             if (Counter < (SlideLength - ItemsOnScreen)) {
-                RightButton.setAttribute("style", `opacity: 1; cursor: pointer`)
-                TranslateX -= WindowSize / ItemsOnScreen
+                TranslateX -= (WindowSize / ItemsOnScreen)
                 Counter++
-                SlideUL.setAttribute("style", `transform: translateX(${TranslateX}px)`)
-            } else {
-                RightButton.setAttribute("style", `opacity: 0.4; cursor: no-drop`)
+                SlideUL.setAttribute("style", `transform: translateX(${TranslateX - (Margin + (Margin/ItemsOnScreen))}px)`)
             }
         })
 
         LeftButton.addEventListener('click', function () {
-            RightButton.setAttribute("style", `opacity: 1; cursor: pointer`)
             if (Counter > 0 ) {
-                TranslateX += WindowSize / ItemsOnScreen
+                TranslateX += (WindowSize / ItemsOnScreen) - (Margin/ItemsOnScreen)
                 Counter--
-                SlideUL.setAttribute("style", `transform: translateX(${TranslateX}px)`)
-            } else {
-                LeftButton.setAttribute("style", `opacity: 0.4; cursor: no-drop`)
+                SlideUL.setAttribute("style", `transform: translateX(${TranslateX + (Margin + (Margin/ItemsOnScreen))}px)`)
             }
         });
     })
